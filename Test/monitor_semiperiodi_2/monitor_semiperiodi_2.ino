31d6b61bd8ae0352a4417c52a8f84b63a98a072c
Duilio Peroni <duilio.peroni@gmail.com>
2017-05-18 09:19:52 +0200

    Add files via upload

---
+++ Test/monitor_semiperiodi_2/monitor_semiperiodi_2.ino
@@ -0,0 +1,72 @@
+//The Gooble Bike 2.0!
+//determina il semiperiodo della forma d'onda del sensore di velocità
+//e lo stampa in unità di campionamento (default 1 mS)
+//il led verde lampeggia in toggle tra i semiperiodi
+//frequenza di campionamento regolabile
+//iDP 14/5/2017
+
+//hw Gooble Bike 1.0
+#define SENSOR  2
+#define BRAKE   5
+#define BRAKING 3
+#define MOVING  9
+#define TCAMP   1
+
+int sensor;
+int previous;
+int status;
+int countLOW=0;
+int countHIGH=0;
+void setup() {
+  pinMode(SENSOR,INPUT);
+  pinMode(BRAKE,OUTPUT);
+  pinMode(BRAKING,OUTPUT);
+  pinMode(MOVING,OUTPUT);
+  digitalWrite(BRAKE,LOW);
+  digitalWrite(BRAKING,LOW);
+  digitalWrite(MOVING,LOW);
+  sensor=digitalRead(SENSOR);
+  previous=sensor;
+  status=sensor;
+  Serial.begin(9600);
+  while(!Serial);
+  Serial.println("The Gooble Bike 2.0: Sensor Monitor semiperiodi");
+  Serial.print("Frequenza di campionamento: ");
+  Serial.println(TCAMP);
+}
+
+void loop() {
+  //campionamento
+  sensor=digitalRead(SENSOR);   //legge sensore di velocità
+  //rilevamento fronti
+  if(sensor!=previous) {        //fronte del segnale
+    if(sensor==HIGH) {          //fronte positivo
+      //stampa conteggio semiperiodo LOW, inizia conteggio HIGH
+      Serial.print("L: ");  
+      Serial.println(countLOW);  
+      status=HIGH;
+      countHIGH=1;
+      digitalWrite(MOVING,HIGH);  //toggle led verde
+    }
+    else { //fronte negativo
+      //stampa conteggio semiperiodo HIGH, inizia conteggio LOW
+      Serial.print("H: ");  
+      Serial.println(countHIGH);  
+      status=LOW;
+      countLOW=1;      
+      digitalWrite(MOVING,LOW); //toggle led verde
+    }
+  }
+  else { //nessun fronte conta TCAMP in base allo stato
+    if (status==HIGH) { //incrementa contatore HIGH
+      countHIGH++; 
+    }
+    else { //incrementa contatore LOW
+      countLOW++;       
+    }
+  }
+  //aggiorna stato precedente
+  previous=sensor;
+  //intervallo di campionamento  
+  delay(TCAMP);
+}
