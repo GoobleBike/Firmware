dfbd24a253d1fb982acae81f95e2fe1920c87d11
Duilio Peroni <duilio.peroni@gmail.com>
2017-05-23 11:49:15 +0200

    cambiato nome da Breake a Brake

---
+++ controllerNew/BrakeActuator.h
@@ -0,0 +1,18 @@
+#ifndef BrakeActuator_h
+#define BrakeActuator_h
+
+
+#include <Arduino.h>
+
+
+class BrakeActuator{
+  private:
+  int port;
+  int led;
+  public:
+  BrakeActuator();
+  void setBrake(unsigned int value);
+};
+#endif
+
+
