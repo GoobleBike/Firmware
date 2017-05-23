d6b3c74f4711e0f5d6e90507afd8a935860b92c0
Duilio Peroni <duilio.peroni@gmail.com>
2017-05-23 11:56:40 +0200

    rimosse globali

--- controllerNew/ReadSpeed.cpp
+++ controllerNew/ReadSpeed.cpp
@@ -1,54 +1,57 @@
 #include "ReadSpeed.h"
 #include "Ports.h"
 
-#define MINSPEED 3 //8 imp/s = 3,6 km/h
-#define MAXSPEED 86 //86 imp/s = 36 km/h
-#define SPEEDTIMEBASE 1000 //timebase = 1 s
+#define MINPULSES 8   //8 imp/s = circa 3,6 km/h
+#define MAXPULSES 86  //86 imp/s = circa 36 km/h
+#define TBASE   1000  //1s timebase sampling
+#define MINSPEED  3   //3 km/h
+#define MAXSPEED  39  //39 km/h
 
-#define TCAMP   1
-#define TBASE   1000
-
-int sensor;
-int previous;
-int countPulses=0;
-long startBase;
-int vel;
+//int sensor;
+//int previous;
+//int countPulses=0;
+//long startBase;
+//int vel;
 
 ReadSpeed::  ReadSpeed(){
-  this->port = PORT_INPUT_READ;
-  this->portLed = PORT_INPUT_READ_LED;
+  this->port = SENSOR_PIN;
+  this->portLed = MOVING_LED_PIN;
   pinMode(this->port, INPUT);
   pinMode(this->portLed, OUTPUT);
 }
 
+void ReadSpeed::begin(){
+  this->startBase=millis();  
+  this->countPulses=0;
+  this->speed=0;
+  digitalWrite(this->portLed, LOW);
+}
+
 void ReadSpeed:: execute(){
+  int vel;
   int velV;
   //verifica scadenza base dei tempi
   if(millis()-startBase>=TBASE) { //base dei tempi scaduta: invia dato
-    countPulses=constrain(countPulses,0,MAXSPEED);
-    vel=map(countPulses,0,MAXSPEED,0,39);
+    this->countPulses=constrain(this->countPulses,0,MAXPULSES);
+    vel=map(this->countPulses,0,MAXPULSES,0,MAXSPEED);
     this->speed = vel; 
-    velV=map(vel,0,39,0,255);
+    velV=map(vel,0,MAXSPEED,0,255);
     analogWrite(this->portLed, velV);
     if(DEBUG_SERIAL){ 
-       Serial.print("Get Request Speed: ");
-       Serial.print(this->speed);
-       Serial.print("\n");
+       Serial.print("COMPUTED SPEED: ");
+       Serial.println(this->speed);
      }  
-    startBase=millis();
-    countPulses=0;
+    this->startBase=millis();
+    this->countPulses=0;
   }
- // digitalWrite(this->portLed, LOW);
   //campionamento
-  sensor=digitalRead(this->port);
+  this->sensor=digitalRead(this->port);
   //rilevamento fronte di salita
-  if ((sensor!=previous)&&(sensor==HIGH)) { //fronte di salita: conta
-    countPulses++;    
+  if ((this->sensor!=this->previous)&&(this->sensor==HIGH)) { //fronte di salita: conta
+    this->countPulses++;    
   }
   //aggiorna stato precedente
-  previous=sensor;
-  //intervallo di campionamento  
-  //delay(TCAMP); 
+  this->previous=this->sensor;
 }
 
 
@@ -58,8 +61,8 @@ int ReadSpeed::getSpeed(){
   
 int ReadSpeed::IsMoving(){
   boolean ris=false;
-  //verifica se la velocitÃƒÂ  supera la soglia minima
-  if(this->speed > MINSPEED) { //velocitÃƒÂ  sopra la soglia minima: in movimento
+  //verifica se la velocità  supera la soglia minima
+  if(this->speed > MINSPEED) { //velocità  sopra la soglia minima: in movimento
     ris=true;
   }
   return ris;
