d0604f4610f33c4d015682425e72c6613a043d48
Duilio Peroni <duilio.peroni@gmail.com>
2017-05-23 11:48:47 +0200

    Cambiato nome da Breake a Brake

---
+++ controllerNew/BrakeActuator.cpp
@@ -0,0 +1,32 @@
+#include "BrakeActuator.h"
+#include "Ports.h"
+
+BrakeActuator::BrakeActuator(){
+    port =  BRAKE_PIN;
+    led =   BRAKING_LED_PIN;
+    pinMode(port, OUTPUT);
+    pinMode(led, OUTPUT);
+}
+
+
+  
+void BrakeActuator::setBrake(unsigned int value){
+    unsigned int brake;
+    if(DEBUG_SERIAL){
+      Serial.print("RCV PEND: ");
+      Serial.print(value);
+     }
+     value = constrain(value,0,20);
+     brake = map(value,0,20,0,255);
+     analogWrite(port, brake);
+     analogWrite(led, brake);
+    if(DEBUG_SERIAL){
+      Serial.print(" CONSRT PEND: ");
+      Serial.print(value);
+      Serial.print(" ACT PWM: ");
+      Serial.println(brake);
+     }
+}
+
+
+
