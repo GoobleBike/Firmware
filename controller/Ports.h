26d7651f8431777401114bdd0bcf8d25c405b0d9
Duilio Peroni <duilio.peroni@gmail.com>
2017-05-25 15:25:59 +0200

    spostato SEND_LED dal pin 10 al pin 12

--- controller/Ports.h
+++ controller/Ports.h
@@ -7,11 +7,11 @@
 #define BRAKE_PIN             5
 #define MOVING_LED_PIN        9
 #define BRAKING_LED_PIN       3
-#define SEND_LED_PIN         10
+#define SEND_LED_PIN         12
 #define SW1_PIN               6
 #define SW2_PIN               7
 #define SW3_PIN               8
 
-#define DEBUG_SERIAL 1
+#define DEBUG_SERIAL 0
 
 
