3276ef3cc79c310a858b09fc966bdf4ab6639f69
Duilio Peroni <duilio.peroni@gmail.com>
2017-06-26 11:52:17 +0200

    Add files via upload

---
+++ Test/test_cadence/test_cadence.ino
@@ -0,0 +1,12 @@
+void setup() {
+  pinMode(11,INPUT_PULLUP);
+  pinMode(13,OUTPUT);
+  Serial.begin(250000);
+}
+
+void loop() {
+  int val=digitalRead(11);
+  digitalWrite(13,val);
+  Serial.println(val);
+  delay(10);
+}
