3957a6fc8ab7d90ebfb81595a737d0739cb9668c
Duilio Peroni <duilio.peroni@gmail.com>
2017-06-26 11:35:25 +0200

    Add files via upload

--- BolognaExperience/controllerAsync2/Ports.h
+++ BolognaExperience/controllerAsync2/Ports.h
@@ -9,9 +9,10 @@
 #define BRAKING_LED_PIN       3
 #define SEND_LED_PIN         12
 #define SW1_PIN               6
-#define SW2_PIN               7
+#define SW2_PIN               10
 #define SW3_PIN               8
+#define CADENCE_PIN          11
 
 #define DEBUG_SERIAL 0
-
+#define CADENCE_ENABLE 0
 
