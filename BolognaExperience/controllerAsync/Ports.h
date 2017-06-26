47859d7b96edd1ad1543d6ac56b384eaf5c52d77
Duilio Peroni <duilio.peroni@gmail.com>
2017-06-26 11:33:05 +0200

    Add files via upload

---
+++ BolognaExperience/controllerAsync/Ports.h
@@ -0,0 +1,18 @@
+//The Gooble Bike 2.0!
+//Versione per Bologna Experience
+//Definizione delle porte
+//23/5/2017
+
+#define SENSOR_PIN            2
+#define BRAKE_PIN             5
+#define MOVING_LED_PIN        9
+#define BRAKING_LED_PIN       3
+#define SEND_LED_PIN         12
+#define SW1_PIN               6
+#define SW2_PIN               10
+#define SW3_PIN               8
+#define CADENCE_PIN          11
+
+#define DEBUG_SERIAL 0
+#define CADENCE_ENABLE 0
+
